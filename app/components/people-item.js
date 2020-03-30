e62c3d1e4cddcc68a4e6a077963c1fc7b647fd8c
b.chebotarev <b.chebotarev@softteco.com>
2020-03-30 09:03:25 +0300

    fix delete item from list

--- app/components/people-item.js
+++ app/components/people-item.js
@@ -1,12 +1,15 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import {tracked} from "@glimmer/tracking";
 
 export default class PeopleItemComponent extends Component {
   @service peopleData;
+  @tracked isShow = true;
 
   @action
   onClickFire() {
     this.peopleData.remove(this.args.person.id);
+    this.isShow = false
   }
 }
